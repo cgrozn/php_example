b5dbf0d532258d082c08014c31d1cdb93af764e0
ÇAĞRI <33495266+cgrozn@users.noreply.github.com>
2019-09-21 23:00:37 +0300

    Create sinavhazirlama_baglan.php

---
+++ sinavhazirlama_baglan.php
@@ -0,0 +1,16 @@
+<?php 
+
+try 
+{
+	$db=new PDO("mysql:host=localhost;dbname=sinavhazirlama; charset=utf8",'root','');
+
+
+
+	//echo "veritabanı bağlantısı başarılı";
+
+} 
+catch (PDOException $e)
+{
+	echo $e->getMessage();
+}
+?>
